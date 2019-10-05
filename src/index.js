b39135cc6256da6a86e009a8e018183b0e25d9b2
Dave_PM <davidpolme3@gmail.com>
2019-10-04 21:29:08 -0500

    Estilos con SASS

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import HelloWorld from './Components/HelloWolld'
 
-ReactDom.render(<HelloWorld/>, document.getElementById(app))
+ReactDom.render(<HelloWorld/>, document.getElementById('app'))
